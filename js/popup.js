75a45f5b6f4777a41e12da4eb0d00804e073cadb
nadarevd <bigboydeku@gmail.com>
2020-01-15 23:41:38 -0500

    contact information works + now emails work + social media

---
+++ js/popup.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    var popup = document.getElementById("signup-popup");
+    var myButton = document.getElementById("myButton");
+    var span = document.getElementsByClassName("exit")[0];
+
+
+    myButton.onclick = function() {
+        popup.style.display = "block";
+
+    }
+    span.onclick = function() {
+        popup.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+
+        }
+        if (event.target == popuptwo) {
+            popuptwo.style.display = "none";
+        }
+
+    }
+
+
+
+}
